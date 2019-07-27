5f56b6cf7dbc9538a46f4c64a83f522c68de7f4b
Roland Illig <roland.illig@gmx.de>
2019-07-27 19:19:05 +0200

    Replace os.Exit during tests
    Without this replacement, GoLand gets confused when it runs the tests
    and reports "No tests run".

--- check_test.go
+++ check_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gopkg.in/check.v1"
+	"os"
 	"testing"
 )
 
@@ -10,4 +11,16 @@ type Suite struct{}
 func Test(t *testing.T) {
 	check.Suite(new(Suite))
 	check.TestingT(t)
+}
+
+func (s *Suite) SetUpTest(c *check.C) {
+	exit = func(code int) {
+		// Tests for code that calls exit must override this
+		// function and check the code.
+		panic("exit was called from a test")
+	}
+}
+
+func (s *Suite) TearDownTest(c *check.C) {
+	exit = os.Exit
 }
