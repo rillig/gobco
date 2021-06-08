rillig/gobco

commit 6506cc1f1f2837ac164d50995c2c67f337bbc388

Make instrumenter.instrument simpler
It's been a nightmare to understand this bloated code, even though it
was not that complex, objectively speaking.

instrumenter.go
@@ -218,27 +218,20 @@ func (i *instrumenter) visitExprs(exprs []ast.Expr) {
 	}
 }
 
-// instrument reads the given file or directory and instruments the code for
-// branch coverage. It then writes the instrumented code into tmpName.
-func (i *instrumenter) instrument(srcName, tmpName string, isDir bool) {
+// instrument modifies the code of the Go package in dir by adding counters for
+// code coverage. If base is given, only that file is instrumented.
+func (i *instrumenter) instrument(dir, base string) {
 	i.fset = token.NewFileSet()
 
-	srcDir := srcName
-	tmpDir := tmpName
-	if !isDir {
-		srcDir = filepath.Dir(srcDir)
-		tmpDir = filepath.Dir(tmpDir)
-	}
-
 	isRelevant := func(info os.FileInfo) bool {
-		return isDir || info.Name() == filepath.Base(srcName)
+		return base == "" || info.Name() == base
 	}
 
-	pkgs, err := parser.ParseDir(i.fset, srcDir, isRelevant, parser.ParseComments)
+	pkgs, err := parser.ParseDir(i.fset, dir, isRelevant, parser.ParseComments)
 	i.check(err)
 
 	for pkgname, pkg := range pkgs {
-		i.instrumentPackage(pkgname, pkg, tmpDir)
+		i.instrumentPackage(pkgname, pkg, dir)
 	}
 }
 

main.go
@@ -161,8 +161,9 @@ func (g *gobco) instrument() {
 	in.coverTest = g.coverTest
 
 	for _, arg := range g.args {
-		tmp := g.tmpSrc(arg.tmpName)
-		in.instrument(arg.argName, tmp, arg.isDir)
+		dir := g.tmpSrc(arg.tmpDir())
+		base := arg.base()
+		in.instrument(dir, base)
 		g.verbosef("Instrumented %s to %s", arg.argName, arg.tmpName)
 	}
 }
@@ -399,13 +400,20 @@ func (r *runenv) tmpSrc(rel string) string {
 }
 
 type argument struct {
-	// as given in the command line
+	// from the command line, using '/' as separator
 	argName string
 
 	// relative to the temporary $GOPATH/src, using forward slashes.
 	tmpName string
 
 	isDir bool
+}
+
+func (a *argument) base() string {
+	if a.isDir {
+		return ""
+	}
+	return path.Base(a.argName)
 }
 
 func (a *argument) srcDir() string {

commit 64c7e0164bcb775f60ad3fdc6a9a1f74badd5184

Report original filenames instead of those from the temporary directory
This regression came from 6506cc1f1f2837ac164d50995c2c67f337bbc388 from
yesterday, which intended to make instrumenter.instrument simpler.

It's not perfect yet since the reported pathnames are absolute, even if
they are in the current directory.

instrumenter.go
@@ -220,22 +220,22 @@ func (i *instrumenter) visitExprs(exprs []ast.Expr) {
 
 // instrument modifies the code of the Go package in dir by adding counters for
 // code coverage. If base is given, only that file is instrumented.
-func (i *instrumenter) instrument(dir, base string) {
+func (i *instrumenter) instrument(srcDir, base, dstDir string) {
 	i.fset = token.NewFileSet()
 
 	isRelevant := func(info os.FileInfo) bool {
 		return base == "" || info.Name() == base
 	}
 
-	pkgs, err := parser.ParseDir(i.fset, dir, isRelevant, parser.ParseComments)
+	pkgs, err := parser.ParseDir(i.fset, srcDir, isRelevant, parser.ParseComments)
 	i.check(err)
 
 	for pkgname, pkg := range pkgs {
-		i.instrumentPackage(pkgname, pkg, dir)
+		i.instrumentPackage(pkgname, pkg, dstDir)
 	}
 }
 
-func (i *instrumenter) instrumentPackage(pkgname string, pkg *ast.Package, tmpDir string) {
+func (i *instrumenter) instrumentPackage(pkgname string, pkg *ast.Package, dstDir string) {
 
 	// Sorting the filenames is only for convenience during debugging.
 	// It doesn't have any effect on the generated code.
@@ -246,13 +246,13 @@ func (i *instrumenter) instrumentPackage(pkgname string, pkg *ast.Package, tmpDi
 	sort.Strings(filenames)
 
 	for _, filename := range filenames {
-		i.instrumentFile(filename, pkg.Files[filename], tmpDir)
+		i.instrumentFile(filename, pkg.Files[filename], dstDir)
 	}
 
-	i.writeGobcoFiles(tmpDir, pkgname)
+	i.writeGobcoFiles(dstDir, pkgname)
 }
 
-func (i *instrumenter) instrumentFile(filename string, astFile *ast.File, tmpDir string) {
+func (i *instrumenter) instrumentFile(filename string, astFile *ast.File, dstDir string) {
 	fileBytes, err := ioutil.ReadFile(filename)
 	i.check(err)
 	i.text = string(fileBytes)
@@ -274,7 +274,7 @@ func (i *instrumenter) instrumentFile(filename string, astFile *ast.File, tmpDir
 
 	var out bytes.Buffer
 	i.check(printer.Fprint(&out, i.fset, astFile))
-	i.writeFile(filepath.Join(tmpDir, filepath.Base(filename)), out.Bytes())
+	i.writeFile(filepath.Join(dstDir, filepath.Base(filename)), out.Bytes())
 }
 
 func (i *instrumenter) instrumentTestMain(astFile *ast.File) {

main.go
@@ -129,6 +129,7 @@ func (g *gobco) classify(arg string) argInfo {
 		relDir := filepath.Join("gopath", relDir)
 		return argInfo{
 			arg:       arg,
+			argDir:    dir,
 			copySrc:   dir,
 			copyDst:   relDir,
 			instrDir:  relDir,
@@ -142,6 +143,7 @@ func (g *gobco) classify(arg string) argInfo {
 		packageDir := filepath.Join(copyDst, moduleRel)
 		return argInfo{
 			arg:       arg,
+			argDir:    dir,
 			copySrc:   moduleRoot,
 			copyDst:   copyDst,
 			instrDir:  packageDir,
@@ -224,9 +226,9 @@ func (g *gobco) instrument() {
 	in.coverTest = g.coverTest
 
 	for _, arg := range g.args {
-		dir := g.file(arg.instrDir)
-		in.instrument(dir, arg.instrFile)
-		g.verbosef("Instrumented %s to %s", arg.arg, arg.instrDir)
+		instrDir := g.file(arg.instrDir)
+		in.instrument(arg.copySrc, arg.instrFile, instrDir)
+		g.verbosef("Instrumented %s to %s", arg.arg, instrDir)
 	}
 }
 
@@ -478,6 +480,9 @@ func (r *logger) verbosef(format string, args ...interface{}) {
 type argInfo struct {
 	// From the command line, using either '/' or '\\' as separator.
 	arg string
+
+	// Either arg if it is a directory, or its containing directory.
+	argDir string
 
 	// The directory that will be copied to the build environment.
 	copySrc string

main_test.go
@@ -19,6 +19,7 @@ func (s *Suite) Test_gobco_parseCommandLine(c *check.C) {
 	c.Check(g.keep, check.Equals, false)
 	c.Check(g.args, check.DeepEquals, []argInfo{{
 		arg:       ".",
+		argDir:    ".",
 		copySrc:   ".",
 		copyDst:   filepath.FromSlash("gopath/src/github.com/rillig/gobco"),
 		instrDir:  filepath.FromSlash("gopath/src/github.com/rillig/gobco"),
@@ -38,6 +39,7 @@ func (s *Suite) Test_gobco_parseCommandLine__keep(c *check.C) {
 	c.Check(g.keep, check.Equals, true)
 	c.Check(g.args, check.DeepEquals, []argInfo{{
 		arg:       ".",
+		argDir:    ".",
 		copySrc:   ".",
 		copyDst:   filepath.FromSlash("gopath/src/github.com/rillig/gobco"),
 		instrDir:  filepath.FromSlash("gopath/src/github.com/rillig/gobco"),
@@ -57,6 +59,7 @@ func (s *Suite) Test_gobco_parseCommandLine__go_test_options(c *check.C) {
 	c.Check(g.goTestArgs, check.DeepEquals, []string{"-vet=off", "help"})
 	c.Check(g.args, check.DeepEquals, []argInfo{{
 		arg:       "pkg",
+		argDir:    ".",
 		copySrc:   ".", // Since 'pkg' is not an (existing) directory.
 		copyDst:   filepath.FromSlash("gopath/src/github.com/rillig/gobco"),
 		instrDir:  filepath.FromSlash("gopath/src/github.com/rillig/gobco"),