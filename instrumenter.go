476f81ec0dc561c61b780dee3e198398125bfee2
Roland Illig <roland.illig@gmx.de>
2022-10-31 21:29:40 +0100

    Sort the instrumenter methods from large to small

--- instrumenter.go
+++ instrumenter.go
@@ -38,99 +38,63 @@ type instrumenter struct {
 	hasTestMain bool
 }
 
-// addCond remembers a condition and returns its internal ID, which is then
-// used as an argument to the gobcoCover function.
-func (i *instrumenter) addCond(start, code string) int {
-	i.conds = append(i.conds, cond{start, code})
-	return len(i.conds) - 1
+// instrument modifies the code of the Go package in dir by adding counters for
+// code coverage. If base is given, only that file is instrumented.
+func (i *instrumenter) instrument(srcDir, base, dstDir string) {
+	i.fset = token.NewFileSet()
+
+	isRelevant := func(info os.FileInfo) bool {
+		return base == "" || info.Name() == base
+	}
+
+	pkgs, err := parser.ParseDir(i.fset, srcDir, isRelevant, parser.ParseComments)
+	i.check(err)
+
+	for pkgname, pkg := range pkgs {
+		i.instrumentPackage(pkgname, pkg, dstDir)
+	}
 }
 
-// wrap returns the given expression surrounded by a function call to
-// gobcoCover and remembers the location and text of the expression,
-// for later generating the table of coverage points.
-func (i *instrumenter) wrap(cond ast.Expr) ast.Expr {
-	if _, ok := cond.(*ast.UnaryExpr); ok {
-		return cond
+func (i *instrumenter) instrumentPackage(pkgname string, pkg *ast.Package, dstDir string) {
+
+	// Sorting the filenames is only for convenience during debugging.
+	// It doesn't have any effect on the generated code.
+	var filenames []string
+	for filename := range pkg.Files {
+		filenames = append(filenames, filename)
 	}
-	return i.wrapText(cond, cond, i.str(cond))
+	sort.Strings(filenames)
+
+	for _, filename := range filenames {
+		i.instrumentFile(filename, pkg.Files[filename], dstDir)
+	}
+
+	i.writeGobcoFiles(dstDir, pkgname)
 }
 
-// wrap returns the expression cond surrounded by a function call to
-// gobcoCover and remembers the location and text of the expression,
-// for later generating the table of coverage points.
-//
-// The expression orig is the one from the actual code, and in case of
-// switch statements may differ from cond, which is the expression to
-// wrap.
-func (i *instrumenter) wrapText(cond, orig ast.Expr, code string) ast.Expr {
-	origStart := i.fset.Position(orig.Pos())
-	if orig.Pos().IsValid() && !strings.HasSuffix(origStart.Filename, ".go") {
-		return cond // don't wrap generated code, such as yacc parsers
+func (i *instrumenter) instrumentFile(filename string, astFile *ast.File, dstDir string) {
+	fileBytes, err := ioutil.ReadFile(filename)
+	i.check(err)
+	i.text = string(fileBytes)
+
+	shouldBuild := func() bool {
+		ctx := build.Context{GOOS: runtime.GOOS, GOARCH: runtime.GOARCH}
+		ok, err := ctx.MatchFile(path.Dir(filename), path.Base(filename))
+		i.check(err)
+		return ok
 	}
 
-	start := i.fset.Position(orig.Pos())
-	idx := i.addCond(start.String(), code)
-
-	return &ast.CallExpr{
-		Fun: ast.NewIdent("gobcoCover"),
-		Args: []ast.Expr{
-			&ast.BasicLit{Kind: token.INT, Value: fmt.Sprint(idx)},
-			cond}}
-}
-
-func (i *instrumenter) visitSwitch(n *ast.SwitchStmt) {
-	tag := n.Tag
-	body := n.Body.List
-	if tag == nil {
-		for _, body := range n.Body.List {
-			i.visitExprs(body.(*ast.CaseClause).List)
-		}
-		return
+	isTest := strings.HasSuffix(filename, "_test.go")
+	if (i.coverTest || !isTest) && shouldBuild() {
+		ast.Inspect(astFile, i.visit)
+	}
+	if isTest {
+		i.instrumentTestMain(astFile)
 	}
 
-	var varname *ast.Ident
-	if n.Init == nil {
-		n.Tag = nil
-
-		varname = i.nextVarname()
-		n.Init = &ast.AssignStmt{
-			Lhs: []ast.Expr{varname},
-			Tok: token.DEFINE,
-			Rhs: []ast.Expr{tag}}
-	} else {
-		init, ok := n.Init.(*ast.AssignStmt)
-		if !ok || len(init.Lhs) != len(init.Rhs) {
-			return
-		}
-
-		varname = i.nextVarname()
-
-		*n = ast.SwitchStmt{Body: &ast.BlockStmt{List: []ast.Stmt{
-			&ast.CaseClause{
-				List: []ast.Expr{ast.NewIdent("true")},
-				Body: []ast.Stmt{
-					init,
-					&ast.AssignStmt{
-						Lhs: []ast.Expr{varname},
-						Tok: token.DEFINE,
-						Rhs: []ast.Expr{n.Tag},
-					},
-					&ast.SwitchStmt{
-						Body: &ast.BlockStmt{List: body},
-					},
-				},
-			},
-		}}}
-	}
-
-	for _, clause := range body {
-		clause := clause.(*ast.CaseClause)
-		for j, expr := range clause.List {
-			eq := ast.BinaryExpr{X: varname, Op: token.EQL, Y: expr}
-			eqlStr := i.strEql(tag, expr)
-			clause.List[j] = i.wrapText(&eq, expr, eqlStr)
-		}
-	}
+	var out bytes.Buffer
+	i.check(printer.Fprint(&out, i.fset, astFile))
+	i.writeFile(filepath.Join(dstDir, filepath.Base(filename)), out.Bytes())
 }
 
 // visit wraps the nodes of an AST to be instrumented by the coverage.
@@ -206,6 +170,122 @@ func (i *instrumenter) visit(n ast.Node) bool {
 	return true
 }
 
+func (i *instrumenter) visitSwitch(n *ast.SwitchStmt) {
+	tag := n.Tag
+	body := n.Body.List
+	if tag == nil {
+		for _, body := range n.Body.List {
+			i.visitExprs(body.(*ast.CaseClause).List)
+		}
+		return
+	}
+
+	var varname *ast.Ident
+	if n.Init == nil {
+		n.Tag = nil
+
+		varname = i.nextVarname()
+		n.Init = &ast.AssignStmt{
+			Lhs: []ast.Expr{varname},
+			Tok: token.DEFINE,
+			Rhs: []ast.Expr{tag}}
+	} else {
+		init, ok := n.Init.(*ast.AssignStmt)
+		if !ok || len(init.Lhs) != len(init.Rhs) {
+			return
+		}
+
+		varname = i.nextVarname()
+
+		*n = ast.SwitchStmt{Body: &ast.BlockStmt{List: []ast.Stmt{
+			&ast.CaseClause{
+				List: []ast.Expr{ast.NewIdent("true")},
+				Body: []ast.Stmt{
+					init,
+					&ast.AssignStmt{
+						Lhs: []ast.Expr{varname},
+						Tok: token.DEFINE,
+						Rhs: []ast.Expr{n.Tag},
+					},
+					&ast.SwitchStmt{
+						Body: &ast.BlockStmt{List: body},
+					},
+				},
+			},
+		}}}
+	}
+
+	for _, clause := range body {
+		clause := clause.(*ast.CaseClause)
+		for j, expr := range clause.List {
+			eq := ast.BinaryExpr{X: varname, Op: token.EQL, Y: expr}
+			eqlStr := i.strEql(tag, expr)
+			clause.List[j] = i.wrapText(&eq, expr, eqlStr)
+		}
+	}
+}
+
+// visitExprs wraps the given expression list for coverage.
+func (i *instrumenter) visitExprs(exprs []ast.Expr) {
+	for idx := range exprs {
+		i.visitExpr(&exprs[idx])
+	}
+}
+
+// visitExpr wraps comparison expressions in a call to gobcoCover, thereby
+// counting how often these expressions are evaluated.
+func (i *instrumenter) visitExpr(exprPtr *ast.Expr) {
+	switch expr := (*exprPtr).(type) {
+	// FIXME: What about the other types of expression?
+	case *ast.BinaryExpr:
+		if expr.Op.Precedence() == token.EQL.Precedence() {
+			*exprPtr = i.wrap(expr)
+		}
+	case *ast.IndexExpr:
+		i.visitExpr(&expr.Index)
+	}
+}
+
+// wrap returns the given expression surrounded by a function call to
+// gobcoCover and remembers the location and text of the expression,
+// for later generating the table of coverage points.
+func (i *instrumenter) wrap(cond ast.Expr) ast.Expr {
+	if _, ok := cond.(*ast.UnaryExpr); ok {
+		return cond
+	}
+	return i.wrapText(cond, cond, i.str(cond))
+}
+
+// wrap returns the expression cond surrounded by a function call to
+// gobcoCover and remembers the location and text of the expression,
+// for later generating the table of coverage points.
+//
+// The expression orig is the one from the actual code, and in case of
+// switch statements may differ from cond, which is the expression to
+// wrap.
+func (i *instrumenter) wrapText(cond, orig ast.Expr, code string) ast.Expr {
+	origStart := i.fset.Position(orig.Pos())
+	if orig.Pos().IsValid() && !strings.HasSuffix(origStart.Filename, ".go") {
+		return cond // don't wrap generated code, such as yacc parsers
+	}
+
+	start := i.fset.Position(orig.Pos())
+	idx := i.addCond(start.String(), code)
+
+	return &ast.CallExpr{
+		Fun: ast.NewIdent("gobcoCover"),
+		Args: []ast.Expr{
+			&ast.BasicLit{Kind: token.INT, Value: fmt.Sprint(idx)},
+			cond}}
+}
+
+// addCond remembers a condition and returns its internal ID, which is then
+// used as an argument to the gobcoCover function.
+func (i *instrumenter) addCond(start, code string) int {
+	i.conds = append(i.conds, cond{start, code})
+	return len(i.conds) - 1
+}
+
 // strEql returns the string representation of (lhs == rhs).
 func (i *instrumenter) strEql(lhs ast.Expr, rhs ast.Expr) string {
 
@@ -239,86 +319,6 @@ func (i *instrumenter) strEql(lhs ast.Expr, rhs ast.Expr) string {
 	return fmt.Sprintf("%s%s%s == %s%s%s",
 		condStr(lp, "("), i.str(lhs), condStr(lp, ")"),
 		condStr(rp, "("), i.str(rhs), condStr(rp, ")"))
-}
-
-// visitExprs wraps the given expression list for coverage.
-func (i *instrumenter) visitExprs(exprs []ast.Expr) {
-	for idx := range exprs {
-		i.visitExpr(&exprs[idx])
-	}
-}
-
-// visitExpr wraps comparison expressions in a call to gobcoCover, thereby
-// counting how often these expressions are evaluated.
-func (i *instrumenter) visitExpr(exprPtr *ast.Expr) {
-	switch expr := (*exprPtr).(type) {
-	// FIXME: What about the other types of expression?
-	case *ast.BinaryExpr:
-		if expr.Op.Precedence() == token.EQL.Precedence() {
-			*exprPtr = i.wrap(expr)
-		}
-	case *ast.IndexExpr:
-		i.visitExpr(&expr.Index)
-	}
-}
-
-// instrument modifies the code of the Go package in dir by adding counters for
-// code coverage. If base is given, only that file is instrumented.
-func (i *instrumenter) instrument(srcDir, base, dstDir string) {
-	i.fset = token.NewFileSet()
-
-	isRelevant := func(info os.FileInfo) bool {
-		return base == "" || info.Name() == base
-	}
-
-	pkgs, err := parser.ParseDir(i.fset, srcDir, isRelevant, parser.ParseComments)
-	i.check(err)
-
-	for pkgname, pkg := range pkgs {
-		i.instrumentPackage(pkgname, pkg, dstDir)
-	}
-}
-
-func (i *instrumenter) instrumentPackage(pkgname string, pkg *ast.Package, dstDir string) {
-
-	// Sorting the filenames is only for convenience during debugging.
-	// It doesn't have any effect on the generated code.
-	var filenames []string
-	for filename := range pkg.Files {
-		filenames = append(filenames, filename)
-	}
-	sort.Strings(filenames)
-
-	for _, filename := range filenames {
-		i.instrumentFile(filename, pkg.Files[filename], dstDir)
-	}
-
-	i.writeGobcoFiles(dstDir, pkgname)
-}
-
-func (i *instrumenter) instrumentFile(filename string, astFile *ast.File, dstDir string) {
-	fileBytes, err := ioutil.ReadFile(filename)
-	i.check(err)
-	i.text = string(fileBytes)
-
-	shouldBuild := func() bool {
-		ctx := build.Context{GOOS: runtime.GOOS, GOARCH: runtime.GOARCH}
-		ok, err := ctx.MatchFile(path.Dir(filename), path.Base(filename))
-		i.check(err)
-		return ok
-	}
-
-	isTest := strings.HasSuffix(filename, "_test.go")
-	if (i.coverTest || !isTest) && shouldBuild() {
-		ast.Inspect(astFile, i.visit)
-	}
-	if isTest {
-		i.instrumentTestMain(astFile)
-	}
-
-	var out bytes.Buffer
-	i.check(printer.Fprint(&out, i.fset, astFile))
-	i.writeFile(filepath.Join(dstDir, filepath.Base(filename)), out.Bytes())
 }
 
 func (i *instrumenter) instrumentTestMain(astFile *ast.File) {
