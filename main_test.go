f2b0cbdd357fdefa9710c87ec6a88667d950b654
Roland Illig <roland.illig@gmx.de>
2022-11-03 23:21:49 +0100

    Merge check_test.go into main_test.go

--- main_test.go
+++ main_test.go
@@ -5,7 +5,76 @@ import (
 	"gopkg.in/check.v1"
 	"os"
 	"strings"
+	"testing"
 )
+
+type Suite struct {
+	out bytes.Buffer
+	err bytes.Buffer
+}
+
+func (s *Suite) Stdout() string {
+	defer s.out.Reset()
+	return s.out.String()
+}
+
+func (s *Suite) Stderr() string {
+	defer s.err.Reset()
+	return s.err.String()
+}
+
+func (s *Suite) newGobco() *gobco {
+	return newGobco(&s.out, &s.err)
+}
+
+func Test(t *testing.T) {
+	check.Suite(new(Suite))
+	check.TestingT(t)
+}
+
+func (s *Suite) SetUpTest(c *check.C) {
+	exit = func(code int) {
+		panic(exited(code))
+	}
+}
+
+func (s *Suite) TearDownTest(c *check.C) {
+
+	if stdout := s.Stdout(); stdout != "" {
+		c.Errorf("%s: unchecked stdout %q", c.TestName(), stdout)
+	}
+
+	if stderr := s.Stderr(); stderr != "" {
+		c.Errorf("%s: unchecked stderr %q", c.TestName(), stderr)
+	}
+
+	exit = os.Exit
+}
+
+func (s *Suite) CheckContains(c *check.C, output, str string) {
+	if !strings.Contains(output, str) {
+		c.Errorf("expected %q in the output, got %q", str, output)
+	}
+}
+
+func (s *Suite) CheckNotContains(c *check.C, output, str string) {
+	if strings.Contains(output, str) {
+		c.Errorf("expected %q to not appear in the output %q", str, output)
+	}
+}
+
+func (s *Suite) RunMain(c *check.C, expectedExitCode int, argv ...string) (stdout, stderr string) {
+	var outBuf bytes.Buffer
+	var errBuf bytes.Buffer
+
+	actualExitCode := gobcoMain(&outBuf, &errBuf, argv...)
+
+	c.Check(actualExitCode, check.Equals, expectedExitCode)
+
+	return outBuf.String(), errBuf.String()
+}
+
+type exited int
 
 func (s *Suite) Test_gobco_parseCommandLine(c *check.C) {
 	g := s.newGobco()
