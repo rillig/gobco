1646bd7bd40c1770e075021252782f94cc1805d7
Roland Illig <roland.illig@gmx.de>
2022-11-06 23:25:10 +0100

    Remove test dependency on check.v1
    Replaced with plain Go tests, for minimalism.

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/rillig/gobco
 
 go 1.16
-
-require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c
--- main_test.go
+++ main_test.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"bytes"
-	"gopkg.in/check.v1"
 	"os"
+	"reflect"
 	"strings"
 	"testing"
 )
 
 type Suite struct {
+	t   *testing.T
 	out bytes.Buffer
 	err bytes.Buffer
+}
+
+func NewSuite(t *testing.T) *Suite {
+	s := Suite{t: t}
+	exit = func(code int) {
+		panic(exited(code))
+	}
+	return &s
 }
 
 func (s *Suite) Stdout() string {
@@ -27,65 +36,80 @@ func (s *Suite) newGobco() *gobco {
 	return newGobco(&s.out, &s.err)
 }
 
-func Test(t *testing.T) {
-	check.Suite(new(Suite))
-	check.TestingT(t)
-}
-
-func (s *Suite) SetUpTest(c *check.C) {
-	exit = func(code int) {
-		panic(exited(code))
-	}
-}
-
-func (s *Suite) TearDownTest(c *check.C) {
+func (s *Suite) TearDownTest() {
 
 	if stdout := s.Stdout(); stdout != "" {
-		c.Errorf("%s: unchecked stdout %q", c.TestName(), stdout)
+		s.t.Errorf("unchecked stdout %q", stdout)
 	}
 
 	if stderr := s.Stderr(); stderr != "" {
-		c.Errorf("%s: unchecked stderr %q", c.TestName(), stderr)
+		s.t.Errorf("unchecked stderr %q", stderr)
 	}
 
 	exit = os.Exit
 }
 
-func (s *Suite) CheckContains(c *check.C, output, str string) {
+func (s *Suite) CheckContains(output, str string) {
 	if !strings.Contains(output, str) {
-		c.Errorf("expected %q in the output, got %q", str, output)
+		s.t.Errorf("expected %q in the output, got %q", str, output)
 	}
 }
 
-func (s *Suite) CheckNotContains(c *check.C, output, str string) {
+func (s *Suite) CheckNotContains(output, str string) {
 	if strings.Contains(output, str) {
-		c.Errorf("expected %q to not appear in the output %q", str, output)
+		s.t.Errorf("expected %q to not appear in the output %q", str, output)
 	}
 }
 
-func (s *Suite) RunMain(c *check.C, expectedExitCode int, argv ...string) (stdout, stderr string) {
+func (s *Suite) CheckEquals(actual, expected interface{}) {
+	if !reflect.DeepEqual(expected, actual) {
+		s.t.Errorf("expected %+v, got %+v", expected, actual)
+	}
+}
+
+func (s *Suite) CheckPanics(action func(), expected interface{}) {
+	ok := true
+	defer func() {
+		if !ok {
+			s.t.Errorf("expected panic %+v, got no panic", expected)
+			return
+		}
+		actual := recover()
+		if actual != nil && reflect.DeepEqual(actual, expected) {
+			return
+		}
+		s.t.Errorf("expected panic %+v, got panic %+v", expected, actual)
+	}()
+	action()
+	ok = false
+}
+
+func (s *Suite) RunMain(expectedExitCode int, argv ...string) (stdout, stderr string) {
 	var outBuf bytes.Buffer
 	var errBuf bytes.Buffer
 
 	actualExitCode := gobcoMain(&outBuf, &errBuf, argv...)
 
-	c.Check(actualExitCode, check.Equals, expectedExitCode)
+	s.CheckEquals(actualExitCode, expectedExitCode)
 
 	return outBuf.String(), errBuf.String()
 }
 
 type exited int
 
-func (s *Suite) Test_gobco_parseCommandLine(c *check.C) {
+func Test_gobco_parseCommandLine(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	g := s.newGobco()
 
 	g.parseCommandLine([]string{"gobco"})
 	tmpModuleDir := g.args[0].copyDst
 
-	c.Check(g.exitCode, check.Equals, 0)
-	c.Check(g.listAll, check.Equals, false)
-	c.Check(g.keep, check.Equals, false)
-	c.Check(g.args, check.DeepEquals, []argInfo{{
+	s.CheckEquals(g.exitCode, 0)
+	s.CheckEquals(g.listAll, false)
+	s.CheckEquals(g.keep, false)
+	s.CheckEquals(g.args, []argInfo{{
 		arg:       ".",
 		argDir:    ".",
 		module:    true,
@@ -96,16 +120,19 @@ func (s *Suite) Test_gobco_parseCommandLine(c *check.C) {
 	}})
 }
 
-func (s *Suite) Test_gobco_parseCommandLine__keep(c *check.C) {
+func Test_gobco_parseCommandLine__keep(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	g := s.newGobco()
 
 	g.parseCommandLine([]string{"gobco", "-keep"})
 	tmpModuleDir := g.args[0].copyDst
 
-	c.Check(g.exitCode, check.Equals, 0)
-	c.Check(g.listAll, check.Equals, false)
-	c.Check(g.keep, check.Equals, true)
-	c.Check(g.args, check.DeepEquals, []argInfo{{
+	s.CheckEquals(g.exitCode, 0)
+	s.CheckEquals(g.listAll, false)
+	s.CheckEquals(g.keep, true)
+	s.CheckEquals(g.args, []argInfo{{
 		arg:       ".",
 		argDir:    ".",
 		module:    true,
@@ -116,16 +143,19 @@ func (s *Suite) Test_gobco_parseCommandLine__keep(c *check.C) {
 	}})
 }
 
-func (s *Suite) Test_gobco_parseCommandLine__go_test_options(c *check.C) {
+func Test_gobco_parseCommandLine__go_test_options(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	g := s.newGobco()
 
 	g.parseCommandLine([]string{"gobco", "-test", "-vet=off", "-test", "help", "pkg"})
 	tmpModuleDir := g.args[0].copyDst
 
-	c.Check(g.exitCode, check.Equals, 0)
-	c.Check(g.listAll, check.Equals, false)
-	c.Check(g.goTestArgs, check.DeepEquals, []string{"-vet=off", "help"})
-	c.Check(g.args, check.DeepEquals, []argInfo{{
+	s.CheckEquals(g.exitCode, 0)
+	s.CheckEquals(g.listAll, false)
+	s.CheckEquals(g.goTestArgs, []string{"-vet=off", "help"})
+	s.CheckEquals(g.args, []argInfo{{
 		arg:       "pkg",
 		argDir:    ".",
 		module:    true,
@@ -136,25 +166,29 @@ func (s *Suite) Test_gobco_parseCommandLine__go_test_options(c *check.C) {
 	}})
 }
 
-func (s *Suite) Test_gobco_parseCommandLine__two_packages(c *check.C) {
+func Test_gobco_parseCommandLine__two_packages(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	var g gobco
 
-	c.Check(
+	s.CheckPanics(
 		func() { g.parseCommandLine([]string{"gobco", "pkg1", "pkg2"}) },
-		check.Panics,
 		"gobco: checking multiple packages doesn't work yet")
 }
 
-func (s *Suite) Test_gobco_parseCommandLine__usage(c *check.C) {
+func Test_gobco_parseCommandLine__usage(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	g := s.newGobco()
 
-	c.Check(
+	s.CheckPanics(
 		func() { g.parseCommandLine([]string{"gobco", "-invalid"}) },
-		check.Panics,
 		exited(2))
 
-	c.Check(s.Stdout(), check.Equals, "")
-	c.Check(s.Stderr(), check.Equals, ""+
+	s.CheckEquals(s.Stdout(), "")
+	s.CheckEquals(s.Stderr(), ""+
 		"flag provided but not defined: -invalid\n"+
 		"usage: gobco [options] package...\n"+
 		"  -cover-test\n"+
@@ -177,17 +211,19 @@ func (s *Suite) Test_gobco_parseCommandLine__usage(c *check.C) {
 		"    \tprint the gobco version\n")
 }
 
-func (s *Suite) Test_gobco_parseCommandLine__help(c *check.C) {
+func Test_gobco_parseCommandLine__help(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	g := newGobco(&stdout, &stderr)
 
-	c.Check(
+	s.CheckPanics(
 		func() { g.parseCommandLine([]string{"gobco", "--help"}) },
-		check.Panics,
 		exited(0))
 
-	c.Check(stdout.String(), check.Equals, ""+
+	s.CheckEquals(stdout.String(), ""+
 		"usage: gobco [options] package...\n"+
 		"  -cover-test\n"+
 		"    \tcover the test code as well\n"+
@@ -207,28 +243,38 @@ func (s *Suite) Test_gobco_parseCommandLine__help(c *check.C) {
 		"    \tshow progress messages\n"+
 		"  -version\n"+
 		"    \tprint the gobco version\n")
-	c.Check(stderr.String(), check.Equals, "")
+	s.CheckEquals(stderr.String(), "")
 }
 
-func (s *Suite) Test_gobco_parseCommandLine__version(c *check.C) {
+func Test_gobco_parseCommandLine__version(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	g := s.newGobco()
 
-	c.Check(
+	s.CheckPanics(
 		func() { g.parseCommandLine([]string{"gobco", "--version"}) },
-		check.Panics,
 		exited(0))
 
-	c.Check(s.Stdout(), check.Equals, version+"\n")
-	c.Check(s.Stderr(), check.Equals, "")
+	s.CheckEquals(s.Stdout(), version+"\n")
+	s.CheckEquals(s.Stderr(), "")
 }
 
-func (s *Suite) Test_gobco_prepareTmp(c *check.C) {
+func Test_gobco_prepareTmp(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	g := s.newGobco()
 
-	c.Check(g.tmpdir, check.Not(check.Equals), "")
+	if g.tmpdir == "" {
+		s.t.Errorf("expected tmpdir to be set")
+	}
 }
 
-func (s *Suite) Test_gobco_instrument(c *check.C) {
+func Test_gobco_instrument(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	g := s.newGobco()
 	g.parseCommandLine([]string{"gobco", "sample"})
 	g.prepareTmp()
@@ -236,7 +282,7 @@ func (s *Suite) Test_gobco_instrument(c *check.C) {
 	g.instrument()
 
 	instrDst := g.file(g.args[0].instrDir)
-	c.Check(listRegularFiles(instrDst), check.DeepEquals, []string{
+	s.CheckEquals(listRegularFiles(instrDst), []string{
 		"foo.go",
 		"foo_test.go",
 		"gobco_fixed.go",
@@ -313,7 +359,10 @@ func Test_gobco_printCond__listAll(t *testing.T) {
 	}
 }
 
-func (s *Suite) Test_gobco_cleanup(c *check.C) {
+func Test_gobco_cleanup(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
+
 	g := s.newGobco()
 	g.parseCommandLine([]string{"gobco", "-verbose", "sample"})
 	g.prepareTmp()
@@ -321,7 +370,7 @@ func (s *Suite) Test_gobco_cleanup(c *check.C) {
 	g.instrument()
 
 	instrDst := g.file(g.args[0].instrDir)
-	c.Check(listRegularFiles(instrDst), check.DeepEquals, []string{
+	s.CheckEquals(listRegularFiles(instrDst), []string{
 		"foo.go",
 		"foo_test.go",
 		"gobco_fixed.go",
@@ -333,58 +382,67 @@ func (s *Suite) Test_gobco_cleanup(c *check.C) {
 	g.cleanUp()
 
 	_, err := os.Stat(instrDst)
-	c.Check(os.IsNotExist(err), check.Equals, true)
+	s.CheckEquals(os.IsNotExist(err), true)
 
 	_ = s.Stdout()
 	_ = s.Stderr()
 }
 
-func (s *Suite) Test_gobcoMain__test_fails(c *check.C) {
+func Test_gobcoMain__test_fails(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
 
 	actualExitCode := gobcoMain(&s.out, &s.err, "gobco", "-verbose", "-keep", "sample")
-	c.Check(actualExitCode, check.Equals, 1)
+	s.CheckEquals(actualExitCode, 1)
 
 	stdout := s.Stdout()
 	stderr := s.Stderr()
 
 	if strings.Contains(stderr, "[build failed]") {
-		c.Fatalf("build failed: %s", stderr)
+		s.t.Fatalf("build failed: %s", stderr)
 	}
 
-	c.Check(stdout, check.Matches, `(?s).*Branch coverage: 5/8.*`)
+	s.CheckContains(stdout, `Branch coverage: 5/8`)
 }
 
-func (s *Suite) Test_gobcoMain__single_file(c *check.C) {
+func Test_gobcoMain__single_file(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
 
 	// "go test" returns 1 because one of the sample tests fails.
-	stdout, stderr := s.RunMain(c, 1, "gobco", "-list-all", "sample/foo.go")
+	stdout, stderr := s.RunMain(1, "gobco", "-list-all", "sample/foo.go")
 
-	s.CheckNotContains(c, stdout, "[build failed]")
-	s.CheckNotContains(c, stderr, "[build failed]")
-	s.CheckContains(c, stdout, "Branch coverage: 5/6")
-	s.CheckContains(c, stdout, "foo.go:4:14: condition \"i < 10\" was 10 times true and once false")
-	s.CheckContains(c, stdout, "foo.go:7:6: condition \"a < 1000\" was 5 times true and once false")
-	s.CheckContains(c, stdout, "foo.go:10:5: condition \"Bar(a) == 10\" was once false but never true")
+	s.CheckNotContains(stdout, "[build failed]")
+	s.CheckNotContains(stderr, "[build failed]")
+	s.CheckContains(stdout, "Branch coverage: 5/6")
+	s.CheckContains(stdout, "foo.go:4:14: condition \"i < 10\" was 10 times true and once false")
+	s.CheckContains(stdout, "foo.go:7:6: condition \"a < 1000\" was 5 times true and once false")
+	s.CheckContains(stdout, "foo.go:10:5: condition \"Bar(a) == 10\" was once false but never true")
 	// There is no condition for sample/random.go since that file
 	// is not mentioned in the command line.
 }
 
-func (s *Suite) Test_gobcoMain__TestMain(c *check.C) {
+func Test_gobcoMain__TestMain(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
 
-	stdout, stderr := s.RunMain(c, 0, "gobco", "-verbose", "testdata/testmain")
+	stdout, stderr := s.RunMain(0, "gobco", "-verbose", "testdata/testmain")
 
-	s.CheckNotContains(c, stdout, "[build failed]")
-	s.CheckContains(c, stdout, "begin original TestMain")
-	s.CheckContains(c, stdout, "end original TestMain")
+	s.CheckNotContains(stdout, "[build failed]")
+	s.CheckContains(stdout, "begin original TestMain")
+	s.CheckContains(stdout, "end original TestMain")
 	_ = stderr
 }
 
-func (s *Suite) Test_gobcoMain__oddeven(c *check.C) {
-	stdout, stderr := s.RunMain(c, 0, "gobco", "testdata/oddeven")
+func Test_gobcoMain__oddeven(t *testing.T) {
+	s := NewSuite(t)
+	defer s.TearDownTest()
 
-	s.CheckContains(c, stdout, "Branch coverage: 0/2")
-	s.CheckContains(c, stdout, "odd.go:4:9: condition \"x%2 != 0\" was never evaluated")
+	stdout, stderr := s.RunMain(0, "gobco", "testdata/oddeven")
+
+	s.CheckContains(stdout, "Branch coverage: 0/2")
+	s.CheckContains(stdout, "odd.go:4:9: condition \"x%2 != 0\" was never evaluated")
 	// The condition in even_test.go is not instrumented since
 	// the main code is the test subject.
-	c.Check(stderr, check.Equals, "")
+	s.CheckEquals(stderr, "")
 }
