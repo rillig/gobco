67a2699ea49524929fc37301500e2bc4ca3e60ff
Roland Illig <roland.illig@gmx.de>
2022-11-08 00:14:13 +0100

    In Travis, run the instrumenter tests as well
    The instrumentation examples for switch statements and type switch
    statements are meaningful tests, so run them.

--- testdata/instrumenter/SwitchStmt_test.go
+++ testdata/instrumenter/SwitchStmt_test.go
@@ -3,6 +3,11 @@ package instrumenter
 import "testing"
 
 func TestSwitchInit(t *testing.T) {
+
+	// TODO: Actually test that instrumenting the switch statements
+	//  preserves the behavior of the code, even in tricky cases of
+	//  implicit scopes.
+
 	switchStmt(7, false, ".a.")
 	switchStmt(9, true, ".b.")
 }
