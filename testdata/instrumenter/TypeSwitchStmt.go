c2c06ead651e3d75fbea550d70c331e5ab9a8eda
Roland Illig <roland.illig@gmx.de>
2022-11-07 23:51:27 +0100

    Document how type switch statements can be instrumented

--- testdata/instrumenter/TypeSwitchStmt.go
+++ testdata/instrumenter/TypeSwitchStmt.go
@@ -1,8 +1,83 @@
 package instrumenter
 
+import (
+	"reflect"
+)
+
 // https://go.dev/ref/spec#Type_switches
 
 // TODO: Add systematic tests.
 
-func typeSwitchStmt() {
+func typeSwitchStmtScopes(value interface{}) string {
+
+	// Gobco does not instrument type switch statements, as rewriting them
+	// requires more code changes than for ordinary switch statements. It's
+	// pretty straightforward though, see typeSwitchStmtScopesInstrumented
+	// below for a possible approach.
+
+	switch v := value.(type) {
+
+	case int, uint:
+		// In a clause that lists multiple types, the expression 'v' has the
+		// type of the switch tag, in this case 'interface{}'.
+		return "integer " + reflect.TypeOf(v).String()
+
+	case string:
+		// In a clause that lists a single type, the expression 'v' has the
+		// type from the case clause.
+		return "string " + reflect.TypeOf(v).String()
+
+	case struct{}:
+		return "struct{} " + reflect.TypeOf(v).String()
+
+	case uint8:
+		// The variable 'v' may be unused in some of the case clauses.
+		return "byte"
+
+	default:
+		return "other " + reflect.TypeOf(v).String()
+	}
+}
+
+func typeSwitchStmtScopesInstrumented(value interface{}) string {
+
+	// This is how the type switch statement from typeSwitchStmtScopes
+	// could be instrumented.
+	//
+	// As with an ordinary switch statement, the implicit scopes need to be
+	// modeled correctly:
+	//
+	// The outer scope is for the initialization statement and the tag
+	// expression (tmp0).
+	//
+	// The inner scope is per case clause and contains the expression from
+	// the switch statement, converted to the proper type.
+
+	switch tmp0 := value; {
+
+	case func() bool { _, ok := tmp0.(int); return ok }(),
+		func() bool { _, ok := tmp0.(uint); return ok }():
+		v := tmp0
+		_ = v
+		return "integer " + reflect.TypeOf(v).String()
+
+	case func() bool { _, ok := tmp0.(string); return ok }():
+		v := tmp0.(string)
+		_ = v
+		return "string " + reflect.TypeOf(v).String()
+
+	case func() bool { _, ok := tmp0.(struct{}); return ok }():
+		v := tmp0.(struct{})
+		_ = v
+		return "struct{} " + reflect.TypeOf(v).String()
+
+	case func() bool { _, ok := tmp0.(uint8); return ok }():
+		v := tmp0.(uint8)
+		_ = v
+		return "byte"
+
+	default:
+		v := tmp0
+		return "other " + reflect.TypeOf(v).String()
+	}
 }
---
+++ testdata/instrumenter/TypeSwitchStmt_test.go
@@ -0,0 +1,57 @@
+package instrumenter
+
+import "testing"
+
+func Test_typeSwitchStmtScopes(t *testing.T) {
+	tests := []struct {
+		name     string
+		value    interface{}
+		expected string
+	}{
+		{
+			"int",
+			0,
+			"integer int",
+		},
+		{
+			"uint",
+			uint(0),
+			"integer uint",
+		},
+		{
+			"string",
+			"",
+			"string string",
+		},
+		{
+			"struct",
+			struct{}{},
+			"struct{} struct {}",
+		},
+		{
+			"byte",
+			uint8(0),
+			"byte",
+		},
+		{
+			"other",
+			0.0,
+			"other float64",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			original := typeSwitchStmtScopes(tt.value)
+			if original != tt.expected {
+				t.Errorf("expected %q, got original %q",
+					tt.expected, original)
+			}
+
+			instrumented := typeSwitchStmtScopesInstrumented(tt.value)
+			if instrumented != tt.expected {
+				t.Errorf("expected %q, got instrumented %q",
+					tt.expected, instrumented)
+			}
+		})
+	}
+}
