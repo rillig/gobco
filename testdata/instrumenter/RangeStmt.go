4e494e24e88ae3f5ea7d6ca20a8c07aad0cf3e72
Roland Illig <roland.illig@gmx.de>
2022-11-12 10:21:30 +0100

    Demonstrate missing instrumentation in range statement

--- testdata/instrumenter/RangeStmt.go
+++ testdata/instrumenter/RangeStmt.go
@@ -6,7 +6,10 @@ package instrumenter
 
 // rangeStmt covers the instrumentation of [ast.RangeStmt], which has the
 // expression fields Key, Value and X.
-func rangeStmt(a rune, b string) bool {
+func rangeStmt(i int) bool {
+	mi := map[bool]int{}
+	ms := map[bool]string{}
+	mr := map[bool]rune{}
 
 	// In a RangeStmt there is no obvious condition, therefore nothing
 	// is wrapped. Maybe it would be possible to distinguish empty and
@@ -16,10 +19,16 @@ func rangeStmt(a rune, b string) bool {
 	// Code that wants to have this check in a specific place can just
 	// manually add a statement before the range statement:
 	//  _ = len(b) > 0
-	for _, r := range b {
-		if r == a {
+	for _, r := range ms[i > 10] { // TODO: instrument
+		if r == mr[i > 11] {
 			return true
 		}
 	}
+
+	// In a range loop using '=', the expressions on the left don't need
+	// to be plain identifiers.
+	for mi[i > 10], mr[i > 11] = range ms[i > 12] { // TODO: instrument
+	}
+
 	return false
 }
