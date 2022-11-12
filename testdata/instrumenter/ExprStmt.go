8c7b277b6ecfe1f73e2ac943f7c5ba988df8d79f
Roland Illig <roland.illig@gmx.de>
2022-11-12 01:30:48 +0100

    Test expression statements systematically

--- testdata/instrumenter/ExprStmt.go
+++ testdata/instrumenter/ExprStmt.go
@@ -2,7 +2,19 @@ package instrumenter
 
 // https://go.dev/ref/spec#Expression_statements
 
-// TODO: Add systematic tests.
+func exprStmt(i int, ch map[bool]<-chan int) {
 
-func exprStmt() {
+	f := func(bool) {}
+
+	// Statement expressions may be parenthesized.
+	f(i > 0)
+	(f(i > 1))
+	(f)(i > 2)
+	((f)(i > 3))
+
+	// TODO: Instrument the receive statements.
+	<-ch[i > 10]
+	(<-ch[i > 11])
+	<-(ch[i > 12])
+	(<-(ch[i > 13]))
 }
