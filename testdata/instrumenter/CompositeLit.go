6c251c35c664808fa22993621d8caa73a3a267fe
Roland Illig <roland.illig@gmx.de>
2022-11-15 21:25:24 +0100

    Demonstrate missing instrumentation in composite literals

--- testdata/instrumenter/CompositeLit.go
+++ testdata/instrumenter/CompositeLit.go
@@ -2,9 +2,18 @@ package instrumenter
 
 // https://go.dev/ref/spec#Composite_literals
 
-// TODO: Add systematic tests.
-
 // CompositeLit covers the instrumentation of [ast.CompositeLit], which has
 // the expression fields Type (only relevant at compile time) and Elts.
-func compositeLit() {
+func compositeLit(i int) {
+
+	// Both keys and values are instrumented.
+	_ = map[bool]bool{
+		i > 0: i > 1, // TODO: instrument
+	}
+
+	// Nested values are instrumented.
+	_ = [][]bool{
+		{i > 2}, // TODO: instrument
+		{i > 3}, // TODO: instrument
+	}
 }
