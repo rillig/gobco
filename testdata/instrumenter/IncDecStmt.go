9cc3e7dfd8de11eb2588c9380fd53022b80f673a
Roland Illig <roland.illig@gmx.de>
2022-11-15 21:58:20 +0100

    Test inc/dec statements

--- testdata/instrumenter/IncDecStmt.go
+++ testdata/instrumenter/IncDecStmt.go
@@ -2,9 +2,21 @@ package instrumenter
 
 // https://go.dev/ref/spec#IncDec_statements
 
-// TODO: Add systematic tests.
-
 // incDecStmt covers the instrumentation of [ast.IncDecStmt], which has the
 // expression field X.
 func incDecStmt() {
+
+	// The expression must be addressable ...
+	i := 0
+	i++
+	i--
+
+	var arr [1]int
+	arr[0]++
+	arr[0]--
+
+	// ... or a map index expression.
+	m := map[bool]int{}
+	m[!true]++
+	m[!false]--
 }
