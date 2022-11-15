d576220f3816c77e92a0555a5fb81e42db6a7211
Roland Illig <roland.illig@gmx.de>
2022-11-15 21:37:37 +0100

    Test function literals

--- testdata/instrumenter/FuncLit.go
+++ testdata/instrumenter/FuncLit.go
@@ -2,9 +2,12 @@ package instrumenter
 
 // https://go.dev/ref/spec#Function_literals
 
-// TODO: Add systematic tests.
-
 // funcLit covers the instrumentation of [ast.FuncLit], which has no
 // expression fields.
 func funcLit() {
+	inner := func(i int) bool {
+		return i > 0
+	}
+	inner(3)
+	inner(-3)
 }
