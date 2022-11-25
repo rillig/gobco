12ef973ca95f60653b79ce207246c984ab0c7d3c
Roland Illig <roland.illig@gmx.de>
2022-11-25 08:17:28 +0100

    Demonstrate missing instrumentation for variable declarations

--- testdata/instrumenter/ValueSpec.go
+++ testdata/instrumenter/ValueSpec.go
@@ -8,6 +8,18 @@ package instrumenter
 // valueSpec covers the instrumentation of [ast.ValueSpec], which contains the
 // expression fields Type (only relevant at compile time) and Values.
 func valueSpec() {
+	var ( // TODO: instrument
+		_ = 1 > 0
+		_ = 0 > 1
+	)
+
+	// No not instrument constant expressions.
+	// Wrapping them with a call to gobcoCover would turn them
+	// non-constant.
+	const (
+		_ = 1 > 0
+		_ = 0 > 1
+	)
 }
 
 // TODO: instrument the initialization of global variables.
