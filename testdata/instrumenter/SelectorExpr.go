c6a777f1e4e7edf194bcd3852f7407ee80e29963
Roland Illig <roland.illig@gmx.de>
2022-11-25 19:49:53 +0100

    Demonstrate missing instrumentation for selector expressions

--- testdata/instrumenter/SelectorExpr.go
+++ testdata/instrumenter/SelectorExpr.go
@@ -7,4 +7,7 @@ package instrumenter
 // selectorExpr covers the instrumentation of [ast.SelectorExpr], which has
 // the expression field X.
 func selectorExpr() {
+	m := map[bool]struct{ a string }{true: {""}}
+
+	_ = m[1 > 0].a // TODO: instrument
 }
