f2d7dc90fed0d306cb6817ea1f8ca05b4afd3c0a
Roland Illig <roland.illig@gmx.de>
2022-11-25 08:27:50 +0100

    Demonstrate missing instrumentation of slice expressions

--- testdata/instrumenter/SliceExpr.go
+++ testdata/instrumenter/SliceExpr.go
@@ -7,4 +7,9 @@ package instrumenter
 // sliceExpr covers the instrumentation of [ast.SliceExpr], which has the
 // expression fields X, Low, High and Max.
 func sliceExpr() {
+	m := map[bool]int{}
+	var slice []int
+	_ = slice[m[11 == 0]:]                      // TODO: instrument
+	_ = slice[:m[21 == 0]]                      // TODO: instrument
+	_ = slice[m[31 == 0]:m[32 == 0]:m[33 == 0]] // TODO: instrument
 }
