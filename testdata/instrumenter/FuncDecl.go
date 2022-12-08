a95e0b01685b6c5b08fa618f55cbb1f3c08f709f
Roland Illig <roland.illig@gmx.de>
2022-12-08 22:57:20 +0100

    Test names of temporary variables

--- testdata/instrumenter/FuncDecl.go
+++ testdata/instrumenter/FuncDecl.go
@@ -7,4 +7,21 @@ package instrumenter
 // funcDecl covers the instrumentation of [ast.FuncDecl], which has no
 // expression fields.
 func funcDecl() {
+
+	// When this switch statement is instrumented, it saves the tag
+	// expression in a temporary variable with a generated name that
+	// is unlikely to conflict with any actually used variable.
+	switch 1 > 0 {
+	}
+}
+
+func funcDecl2() {
+	// The names of the temporary variables are unique per gobco run.
+	switch 2 > 0 {
+	default:
+		_ = func() {
+			switch 3 > 0 {
+			}
+		}
+	}
 }
