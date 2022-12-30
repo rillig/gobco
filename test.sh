67df34cbd2b51940c28a22812c7c66bc327bcaec
Roland Illig <roland.illig@gmx.de>
2022-12-30 19:39:40 +0100

    Move comment from Travis configuration to test program

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 set -eu
 
+# Don't test the subpackages with './...' since the templates in ./templates
+# are not meaningfully testable on their own.
+
 go test -coverprofile=coverage.txt -covermode=count .
 go test ./testdata/instrumenter
 
