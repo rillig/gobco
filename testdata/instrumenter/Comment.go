8cb607bdae9072726eba4e6cc5513f9d19e05c14
Roland Illig <roland.illig@gmx.de>
2023-01-07 13:53:58 +0100

    Demonstrate that gobco still messes up the comments

--- testdata/instrumenter/Comment.go
+++ testdata/instrumenter/Comment.go
@@ -19,6 +19,17 @@ import _ "embed"
 func comment() {
 	// TODO: Try to move the 'go:embed' comment away from its variable
 	//  declaration, so that it becomes ignored.
+
+	// When gobco instruments a type switch statement, it moves the type
+	// expressions further up in the code but keeps the position
+	// information from the original type expression.
+	switch interface{}(nil).(type) {
+	case int:
+		// comment int
+	case [][][]int:
+		// comment int-3D
+	}
+	// comment after switch
 }
 
 //go:embed Comment.go
