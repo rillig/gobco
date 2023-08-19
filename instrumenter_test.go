77cc5222fbb29d191595e21cc43da25dfe830a20
Roland Illig <roland.illig@gmx.de>
2023-08-19 20:46:02 +0200

    Extract the code for testing the instrumenter to an inner function
    This change allows testing the instrumenter in different modes, creating
    a separate output file for each mode.

--- instrumenter_test.go
+++ instrumenter_test.go
@@ -55,66 +55,70 @@ func Test_instrumenter(t *testing.T) {
 		{"ValueSpec"},
 	}
 
+	testInstrumenter := func(name string, ext string) {
+		base := "testdata/instrumenter/" + name
+
+		goBytes, err := os.ReadFile(base + ".go")
+		if err != nil {
+			t.Fatal(err)
+		}
+		src := string(goBytes)
+
+		expectedBytes, err := os.ReadFile(base + ext)
+		if err != nil {
+			expectedBytes = nil
+		}
+		expected := string(expectedBytes)
+
+		fset := token.NewFileSet()
+		mode := parser.ParseComments
+		f, err := parser.ParseFile(fset, "test.go", src, mode)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		i := instrumenter{
+			false,
+			false,
+			false,
+			fset,
+			0,
+			map[ast.Node]bool{},
+			map[ast.Expr]*exprSubst{},
+			map[ast.Stmt]*ast.Stmt{},
+			map[ast.Stmt]ast.Stmt{},
+			false,
+			nil,
+		}
+		i.instrumentFileNode(f)
+
+		var sb strings.Builder
+		err = printer.Fprint(&sb, fset, f)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(i.conds) > 0 {
+			sb.WriteString("\n")
+		}
+		for _, cond := range i.conds {
+			location := strings.TrimPrefix(cond.pos, "test.go")
+			sb.WriteString(fmt.Sprintf("// %s: %q\n",
+				location, cond.text))
+		}
+		actual := sb.String()
+
+		if actual != expected {
+			err := os.WriteFile(base+ext, []byte(actual), 0o666)
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Errorf("expected:\n%s\nactual:\n%s\n", expected, actual)
+		}
+	}
+
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			base := "testdata/instrumenter/" + test.name
-
-			goBytes, err := os.ReadFile(base + ".go")
-			if err != nil {
-				panic(err)
-			}
-			src := string(goBytes)
-
-			gobcoBytes, err := os.ReadFile(base + ".gobco")
-			if err != nil {
-				panic(err)
-			}
-			expected := string(gobcoBytes)
-
-			fset := token.NewFileSet()
-			mode := parser.ParseComments
-			f, err := parser.ParseFile(fset, "test.go", src, mode)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			i := instrumenter{
-				false,
-				false,
-				false,
-				fset,
-				0,
-				map[ast.Node]bool{},
-				map[ast.Expr]*exprSubst{},
-				map[ast.Stmt]*ast.Stmt{},
-				map[ast.Stmt]ast.Stmt{},
-				false,
-				nil,
-			}
-			i.instrumentFileNode(f)
-
-			var sb strings.Builder
-			err = printer.Fprint(&sb, fset, f)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if len(i.conds) > 0 {
-				sb.WriteString("\n")
-			}
-			for _, cond := range i.conds {
-				location := strings.TrimPrefix(cond.pos, "test.go")
-				sb.WriteString(fmt.Sprintf("// %s: %q\n",
-					location, cond.text))
-			}
-			actual := sb.String()
-
-			if actual != expected {
-				err := os.WriteFile(base+".gobco", []byte(actual), 0o666)
-				if err != nil {
-					t.Fatal(err)
-				}
-				t.Errorf("expected:\n%s\nactual:\n%s\n", expected, actual)
-			}
+			testInstrumenter(test.name, ".gobco")
 		})
 	}
 }
