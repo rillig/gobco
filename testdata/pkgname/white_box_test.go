48562883eb817ae26572b27d8cad603ce428593f
Roland Illig <roland.illig@gmx.de>
2023-10-22 22:30:57 +0200

    Fix typo in comment

--- testdata/pkgname/white_box_test.go
+++ testdata/pkgname/white_box_test.go
@@ -2,7 +2,7 @@ package pkgname
 
 import "testing"
 
-// In a black box test, the package name is the same as in the main code
+// In a white box test, the package name is the same as in the main code
 // and therefore can access the unexported members of the main code,
 // as well as the exported ones.
 
